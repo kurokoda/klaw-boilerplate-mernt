cd182d148c6ec8e2ff98ac1b99b853efabd92f78
Kurokoda <indigo.developer@gmail.com>
2018-10-12 13:08:12 -0400

    Restructures src directory
    The create-react-application places its generated files in a flat structure in the
    `src` folder. This commit moves several files and establishes the beginning of a more
    organized directory structure.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App from './component/app';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(
   <App />,
